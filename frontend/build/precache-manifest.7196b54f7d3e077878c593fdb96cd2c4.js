c5b049ca046e3b79847fc61edd5155bf06c8543f
Francisco Sarmiento <frank.jav.sm@gmail.com>
2019-12-04 13:15:12 -0400

    build file commit

---
+++ frontend/build/precache-manifest.7196b54f7d3e077878c593fdb96cd2c4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a817dfc2bd5c88a10f5e66bcb0a913c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d70d98999736ebb36f31",
+    "url": "./static/css/main.9ffda037.chunk.css"
+  },
+  {
+    "revision": "11534f00657871a99c0b",
+    "url": "./static/js/2.a62651d5.chunk.js"
+  },
+  {
+    "revision": "d70d98999736ebb36f31",
+    "url": "./static/js/main.9792cdc9.chunk.js"
+  },
+  {
+    "revision": "52d0bb6d076851d61d88",
+    "url": "./static/js/runtime-main.f23cecf6.js"
+  },
+  {
+    "revision": "fcac2dd0845b381fd34f0d902dd6e87b",
+    "url": "./static/media/Poppins-Light.fcac2dd0.ttf"
+  },
+  {
+    "revision": "28bc024aa3164e0b0ff2d709f17b333f",
+    "url": "./static/media/Poppins-Medium.28bc024a.ttf"
+  },
+  {
+    "revision": "dd1aed50244d7243b9209dc901c8d1af",
+    "url": "./static/media/Poppins-Regular.dd1aed50.ttf"
+  },
+  {
+    "revision": "ca29a3f5872103bd3793a033976a04d7",
+    "url": "./static/media/Poppins-Thin.ca29a3f5.ttf"
+  },
+  {
+    "revision": "978a276df404887c6af9609ad5f80cee",
+    "url": "./static/media/check-circle.978a276d.svg"
+  },
+  {
+    "revision": "556bdecee56c1cd46947a93e1c3437ef",
+    "url": "./static/media/file.556bdece.svg"
+  },
+  {
+    "revision": "44e6e96cd371bcb62d3af66aeadf1e00",
+    "url": "./static/media/info.44e6e96c.svg"
+  },
+  {
+    "revision": "289864ba71cbea5128ff761865f986da",
+    "url": "./static/media/logout.289864ba.svg"
+  },
+  {
+    "revision": "2e91abe3f034c75b81749b974bd37d0a",
+    "url": "./static/media/metamask.2e91abe3.png"
+  },
+  {
+    "revision": "2f3bb4e2c4366aa77bf4da0da0fd5f12",
+    "url": "./static/media/plus.2f3bb4e2.svg"
+  },
+  {
+    "revision": "a47fec594d0909c3348892c2dfe782d5",
+    "url": "./static/media/trash.a47fec59.svg"
+  }
+]);
